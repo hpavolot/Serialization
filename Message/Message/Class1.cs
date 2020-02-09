3e7e0854e80844ca9830a3b109e22192f2f1a30f
Rustam Aliyev <helen.pavolotska@gmail.com>
2020-02-09 21:34:06 +0000

    First test with serialization

---
+++ Message/Message/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Message
+{
+    using System;
+
+    [Serializable]
+    public class Message
+    {
+        public string Name { get; set; }
+
+        public Message(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace ClientServer
+{
+    class Server
+    {
+
+        public static Message.Message message = null;
+        public static void StartListening()
+        {
+            byte[] bytes = new byte[1024];
+
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(2);
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection...");
+
+                    Socket handler = listener.Accept();
+
+                    MemoryStream stream = new MemoryStream();
+                    BinaryFormatter formatter = new BinaryFormatter();
+
+                    //formatter.Context = new StreamingContext(StreamingContextStates.Clone);
+
+                    int byteRec = handler.Receive(bytes);
+                    stream.Write(bytes, 0, byteRec);
+
+                    stream.Position = 0;
+
+                    message = (Message.Message)formatter.Deserialize(stream);
+                    Console.WriteLine("Name: {0}", message.Name);
+
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            StartListening();
+        }
+    }
+}
